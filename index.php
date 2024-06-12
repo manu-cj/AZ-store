b830d83bf8bb2d3cf9a0bfcfbcc6972ebca9d14c
Giovanni_T <giovannitummino.15@gmail.com>
2024-06-12 12:28:51 +0200

    mise a jour

--- index.php
+++ index.php
@@ -8,49 +8,36 @@
     <title>AZ_Store</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand spacer" href="#">AZ[store]</a>
+<nav class="navbar navbar-expand-lg">
+  <div class="container-fluid justify-content-between">
+    <a class="navbar-brand spacer custom-text-color" href="#">AZ[store]</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="?c=cart">cart</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        <a class="nav-link disabled" aria-disabled="true">Login</a>
+        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Product</a>
+        <a class="nav-link" href="#">Contact</a>
+        <a class="nav-link disabled login" aria-disabled="true">Login</a>
       </div>
     </div>
   </div>
 </nav>
-    <?php
-
-    $c = $_GET['c'];
-    $a = $_GET['a'];
-   
-    function getPath($page) {
-    
-      require __DIR__ . "/pages/".$page.".php";
-  }
-
-  if (!isset($c)) {
-    header("LOCATION: ?c=home");
-  }
-    switch ($c) {
-      case 'home':
-        getPath('home');
-        break;
-      case 'cart':
-        getPath('cart');
-        break;
-      // pour ajouter une page rajouter un case en suivant les exemples précèdents
-      default:
-        getPath('404');
-        break;
-    }
-    ?>
-   
+    <main>
+      <div class="shoe">
+        <p>
+        SHOE THE
+        </p>
+        <p>RIGHT <span class="one">ONE</span>.
+        </p>
+        <button class="btn btn-primary btn-sm">See our store</button>
+      </div>
+      <div class="nike">
+        <img class="shoe1" src="shoe_one.png">
+        NIKE
+      </div>
+    </main>
 </body>
 </html>
