37ecd908fb42dce3884fe14334e997ece76e8cc3
Justine-Frigo <justine0912@gmail.com>
2024-06-12 21:08:36 +0200

    fix cart + routes + turned session array into associative one

--- controller/addToCart.php
+++ controller/addToCart.php
@@ -4,24 +4,20 @@ $productsJson = file_get_contents('real_products.json');
 $products = json_decode($productsJson, true);
 
 if (isset($_POST['add-to-cart'])) {
-    $id = strip_tags($_POST['article-id']);
+  var_dump('coucou');
+  $id = strip_tags($_POST['article-id']);
 
-    $productId = $products[$id-1]['id'];
-    $product = $products[$id-1]['product'];
-    $price = $products[$id-1]['price'];
-    $imageUrl = $products[$id-1]['image_url'];
-   
+  $productId = $products[$id - 1]['id'];
+  $product = $products[$id - 1]['product'];
+  $price = $products[$id - 1]['price'];
 
-      
-        $cartProduct = array($id, $product, $price, $imgProduct);
-      
-        if (!isset($_SESSION['product-datas'])) {
-          $_SESSION['product-datas'] = [];
-        }
+  $cartProduct = array('id' => $id, 'name' => $product, 'price' => $price, 'quantity' => 1);
 
-        $_SESSION['product-datas'][] = $cartProduct;
-      
+  if (!isset($_SESSION['product-data'])) {
+    $_SESSION['product-data'] = [];
+  }
 
-      header("LOCATION: ?c=products");
+  $_SESSION['product-data'][] = $cartProduct;
+
+  header("LOCATION: ?c=products");
 }
-?>
--- index.php
+++ index.php
@@ -1,78 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="main.css"rel="stylesheet">
-    <link href="assets/css/style.css"rel="stylesheet">
-    <title>AZ_Store</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="main.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <title>AZ_Store</title>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid justify-content-between">
-    <a class="navbar-brand spacer custom-text-color" href="#">AZ[store]</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
-        <a class="nav-link" href="?c=products">Product</a>
-        <a class="nav-link" href="?c=cart">Cart</a>
-        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        <a class="nav-link disabled" aria-disabled="true">Login</a>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid justify-content-between">
+      <a class="navbar-brand spacer custom-text-color" href="?c=home">AZ[store]</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <a class="nav-link active" aria-current="page" href="?c=home">Home</a>
+          <a class="nav-link" href="?c=products">Product</a>
+          <a class="nav-link" href="?c=cart">Cart</a>
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link disabled" aria-disabled="true">Login</a>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
-    <main>
-      <div class="shoe">
-        <p>
+  </nav>
+  <main>
+    <div class="shoe">
+      <p>
         SHOE THE
-        </p>
-        <p>RIGHT <span class="one">ONE</span>.
-        </p>
-        <button class="btn btn-primary btn-sm">See our store</button>
-      </div>
-      <div class="nike">
-        <img class="shoe1" src="shoe_one.png">
-        NIKE
-      </div>
-    </main>
-    <?php
-    session_start();
-    $c = $_GET['c'];
-    $a = $_GET['a'];
-   
-    function getPath($page) {
-    
-      require __DIR__ . "/".$page.".php";
+      </p>
+      <p>RIGHT <span class="one">ONE</span>.
+      </p>
+      <button class="btn btn-primary btn-sm">See our store</button>
+    </div>
+    <div class="nike">
+      <img class="shoe1" src="shoe_one.png">
+      NIKE
+    </div>
+  </main>
+  <?php
+  session_start();
+  $c = $_GET['c'];
+  $a = $_GET['a'] ?? null;
+
+  function getPath($page)
+  {
+    require __DIR__ . "/" . $page . ".php";
   }
 
   if (!isset($c)) {
     header("LOCATION: ?c=home");
   }
-    switch ($c) {
-      case 'home':
-        getPath('pages/home');
-        break;
-      case 'cart':
-        getPath('pages/cart');
-        break;
-      case 'products':
-        getPath('pages/products');
-        break;
-      case 'add-to-cart':
-        getPath('controller/addToCart');
-        break;
+  switch ($c) {
+    case 'home':
+      getPath('pages/home');
+      break;
+    case 'cart':
+      getPath('pages/cart');
+      break;
+    case 'products':
+      getPath('pages/products');
+      break;
+    case 'add-to-cart':
+      getPath('controller/addToCart');
+      break;
+
+    case 'shipping-address':
+      getPath('pages/shippingAddress');
+      break;
       // pour ajouter une page rajouter un case en suivant les exemples précèdents
-      default:
-        getPath('404');
-        break;
-    }
-    
-    ?>
-   
+    default:
+      getPath('pages/404');
+      break;
+  }
+
+  ?>
+
 </body>
+
 </html>
